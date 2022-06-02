ba0d7cfb34dde91b3f69b58ffcadb7a7c9fb6be9
tamarazoppellari <tamistoyota@gmail.com>
2022-06-01 21:21:15 -0300

    Add description to loan_proposal

--- app/controllers/loan_proposals_controller.rb
+++ app/controllers/loan_proposals_controller.rb
@@ -2,19 +2,32 @@ class LoanProposalsController < ApplicationController
 
   def new
     @loan_proposal = LoanProposal.new
-    autorize @loan_proposal
+    authorize @loan_proposal
   end
 
   def create
+    @loan_proposal = LoanProposal.new(loan_proposal_params)
+    @loan_proposal.user = current_user
+    if @loan_proposal.save
+      redirect_to user_profile_path(@loan_proposal), notice: "Proposta de empréstimo realizada com sucesso"
+    else
+      render :new
+    end
+    authorize @loan_proposal
   end
 
   def show
+
   end
 
   def edit
   end
 
   def update
+    @loan_proposal.update(loan_proposal_params)
+    redirect_to loan_proposal_path(@loan_proposal)
+    authorize @loan_proposal
+
   end
 
   def destroy
---
+++ app/policies/loan_proposal_policy.rb
@@ -0,0 +1,20 @@
+class LoanProposalPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+     def resolve
+       scope.all
+     end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+end
